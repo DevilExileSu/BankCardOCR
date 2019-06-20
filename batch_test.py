e4ac0075d633a3e76724e98ccd6f9f2ed739ec66
DevilExileSu <39342795+DevilExileSu@users.noreply.github.com>
2019-06-20 22:29:19 +0800

    Update batch_test.py

--- batch_test.py
+++ batch_test.py
@@ -7,7 +7,7 @@ import keras
 
 
 test_dir = 'test_images/'
-res_dir = 'dtest_results/'
+res_dir = 'test_results/'
 
 char2num_dict = {'0': 0, '1': 1, '2':2, '3': 3, 
                 '4': 4, '5': 5, '6': 6, '7': 7, 
